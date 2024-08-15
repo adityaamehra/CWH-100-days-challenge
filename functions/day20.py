39fb54a01cf8cbaa6d9029b8826448d8a39e6b34
Adityaa Mehra <adirakash@gmail.com>
2024-08-15 22:26:06 +0530

    starting

---
+++ functions/day20.py
@@ -0,0 +1,18 @@
+def gmean(a,b):
+    gmean = ((a*b)**(0.5))
+    print(gmean)
+
+def t(a,b):
+    if(a==b):
+        print("Same")
+    elif(a>b):
+        print(a,"is greater than",b)
+    else:
+        print(b,"is greater than",a)
+
+for i in range((int)(input("Enter the range "))):
+    a=(int)(input("Enter 1st number "))
+    b=(int)(input("Enter 2nd number "))
+    gmean(a,b)
+    t(a,b)
+
