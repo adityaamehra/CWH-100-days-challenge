537acb94b3390d59535d060a9e36b6f6e1748c24
Adityaa Mehra <adirakash@gmail.com>
2024-08-15 07:59:38 +0530

    commit 2

--- while loop/day18.py
+++ while loop/day18.py
@@ -1,12 +1,15 @@
 i=0
-while(i<=38):
-    i=(int)(input("Enter the number "))
-    print(i)
+# while(i<=38):
+#     i=(int)(input("Enter the number "))
+#     print(i)
+# i=(int)(input("Enter the number "))
+# while(i<3):
+#     print(i)
+#     i=i+1 
 i=(int)(input("Enter the number "))
-while(i<3):
-    print(i)
-    i=i+1 
-i=(int)(input("Enter the number "))
+
 while(i>=0):
     print(i)
     i=i-1
+else:
+    print("I am inside else")
