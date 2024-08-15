ce361efe4b92f341613595883806ab0b9074db8a
Adityaa Mehra <adirakash@gmail.com>
2024-08-15 22:59:05 +0530

    final

--- functions/day20.py
+++ functions/day20.py
@@ -10,6 +10,9 @@ def t(a,b):
     else:
         print(b,"is greater than",a)
 
+# pass functions , THEY WILL BE WRITTEN AFTERWARDS
+def passf(a,b,c):
+    pass
 for i in range((int)(input("Enter the range "))):
     a=(int)(input("Enter 1st number "))
     b=(int)(input("Enter 2nd number "))
