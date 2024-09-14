627478140f3861e9328c7ed4d897c937ab6f1453
Adityaa Mehra <adirakash@gmail.com>
2024-09-15 03:13:17 +0530

    final

--- sets/day31.py
+++ sets/day31.py
@@ -1,5 +1,10 @@
+#SETS HAVE AN UNORDERED TYPE OF DATA STORAGE
 #Set does not take repeated values
 #Set is a set of objects
-s={2,4,2,6}
-s2={1,}
+s={2,6,2,4}
+#To make an empty set
+s2=set()
 print(type(s2))
+
+for x in s:
+    print(x,end=' ')
