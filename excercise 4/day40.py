ea396aeec98c24a055de9784ebbc803dfd6a2415
Adityaa Mehra <adirakash@gmail.com>
2024-09-17 16:47:20 +0530

    final

--- excercise 4/day40.py
+++ excercise 4/day40.py
@@ -1,14 +1,22 @@
 s=input("Enter the message\n")
 a=int(input("Do you want to encode or decode the message\n"))
 if a==1:
-    if len(s)<=4 :
-        s2=s[::-1]
-    else:
-        s2="wce"+s[1:]+s[0]+"wce"
-    print(f"The encoded message is : {s2}")
+    c=""
+    b=s.split(" ")
+    for x in b:
+        if len(x)<=4 :
+            s2=x[::-1]
+        else:
+            s2="axc"+x[1:]+x[0]+"wuk"
+        c+=" "+s2
+    print(f"The encoded sentence is {c}")
 elif a==2:
-    if len(s)<=4 :
-        s2=s[::-1]
-    else:
-        s2=s[-4:-3]+s[3:-4]
-    print(f"The decoded message is : {s2}")
+    c=""
+    b=s.split(" ")
+    for x in b:
+        if len(x)<=4 :
+            s2=x[::-1]
+        else:
+            s2=x[-4:-3]+x[3:-4]
+        c+=" "+s2
+    print(f"The decoded message is : {c}")
