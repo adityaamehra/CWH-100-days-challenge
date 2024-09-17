2d79749ed4ce3879239aabd40fe027b41ecfa3b7
Adityaa Mehra <adirakash@gmail.com>
2024-09-17 16:54:57 +0530

    final

---
+++ short hand if and else/day41.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first value\n"))
+b=int(input("Enter the second value\n"))
+print("A") if a>b else print("B") if a==b else print("C")
+
+print("D") if a>10*b else ""
+
+c=1 if a>b else 10
